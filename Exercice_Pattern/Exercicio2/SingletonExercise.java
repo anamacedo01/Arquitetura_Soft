cc06d6be14337e30aeb04c45d332f49be5a2653d
anamacedo01 <anapaulasilvademacedo@yahoo.com.br>
2018-06-03 16:53:36 -0300

    Aula07_parte2

---
+++ Exercice_Pattern/Exercicio2/SingletonExercise.java
@@ -0,0 +1,17 @@
+package Exercicio2;
+
+/**
+ * 
+ * @author Ana Paula Macedo 816114287
+ * 			
+ *
+ */
+
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
